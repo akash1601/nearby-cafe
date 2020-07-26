0f6c979ba0e59a3daf6212ddd1d5fddd40a56c98
akash1601 <akashshah1601@gmail.com>
2020-07-26 12:58:52 -0700

    Added home screen

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import { StyleSheet, Text, SafeAreaView } from 'react-native'
 import { HomeScreen } from './app/screens'
+import Constants from 'expo-constants';
+import 'react-native-gesture-handler';
 
 export default class App extends React.Component {
+  state = {
+    showApp: false,
+  }
+  showApp = () => {
+
+     this.setState({showApp: true})
+   }
 	render() {
+    if (this.state.showApp) return 	<HomeScreen />
+			
+		
 		return (
-			<SafeAreaView style={styles.container}>
-				<HomeScreen />
-			</SafeAreaView>
-		)
+			<View style={styles.container}>
+      <Button title = "Nearby Cafes and Restaurants" onPress={this.showApp}/>
+      </View>
+		);
 	}
 }
+
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
+    marginTop: 100,
+ 
 		justifyContent: 'flex-start'
 	}
 })
